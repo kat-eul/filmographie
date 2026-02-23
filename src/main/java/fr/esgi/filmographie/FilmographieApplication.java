0d5615db26ad4fea37ccb89962e5d85c7f6f54b8
Adrien DHOMMEAUX <adrien.dhommeaux@gmail.com>
2026-02-23 15:15:52 +0100

    chore: change package name

--- src/main/java/fr/esgi/filmographie/FilmographieApplication.java
+++ src/main/java/fr/esgi/filmographie/FilmographieApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package fr.esgi.filmographie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/fr/esgi/filmographie/FilmographieApplicationTests.java
+++ src/test/java/fr/esgi/filmographie/FilmographieApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package fr.esgi.filmographie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
