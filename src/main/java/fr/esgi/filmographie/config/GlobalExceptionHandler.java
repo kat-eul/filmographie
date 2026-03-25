f6dd9c67dc9816e68c3e773113e53cc205abb19a
kat-eul <katel.hg@proton.me>
2026-03-25 11:21:48 +0100

    fix : gestion exception

--- src/main/java/fr/esgi/filmographie/config/GlobalExceptionHandler.java
+++ src/main/java/fr/esgi/filmographie/config/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package fr.esgi.filmographie.config;
 
+import fr.esgi.filmographie.exception.NotFoundException;
 import fr.esgi.filmographie.movie.exception.MovieNotFoundException;
-import fr.esgi.filmographie.role.exception.RoleNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,13 +10,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MovieNotFoundException.class)
-    public ResponseEntity<String> handleMovieNotFoundException(MovieNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
-
-    @ExceptionHandler(RoleNotFoundException.class)
-    public ResponseEntity<String> handleRoleNotFoundException(RoleNotFoundException ex) {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
