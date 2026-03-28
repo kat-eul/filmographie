5be7fbcb8b2a58fcb9e72d2ad195a02d237b710d
matteobelin <122531813+matteobelin@users.noreply.github.com>
2026-03-28 12:16:49 +0100

    Rajout du get by job

--- src/main/java/fr/esgi/filmographie/enums/JobEnum.java
+++ src/main/java/fr/esgi/filmographie/enums/JobEnum.java
@@ -3,12 +3,21 @@ package fr.esgi.filmographie.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 @Getter
 @AllArgsConstructor
 public enum JobEnum {
-    ACTOR("Actor"),
-    REALISATOR("Realisator"),
-    REALISATOR_ACTOR("Realisator and Actor");
+    ACTOR,
+    REALISATOR,
+    REALISATOR_ACTOR;
 
-    private final String jobName;
+    public Set<JobEnum> includedForFilter() {
+        return switch (this) {
+            case ACTOR -> EnumSet.of(ACTOR, REALISATOR_ACTOR);
+            case REALISATOR -> EnumSet.of(REALISATOR, REALISATOR_ACTOR);
+            case REALISATOR_ACTOR -> EnumSet.of(REALISATOR_ACTOR);
+        };
+    }
 }
--- src/main/java/fr/esgi/filmographie/person/PersonRepository.java
+++ src/main/java/fr/esgi/filmographie/person/PersonRepository.java
@@ -1,6 +1,11 @@
 package fr.esgi.filmographie.person;
 
+import fr.esgi.filmographie.enums.JobEnum;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface PersonRepository extends ListCrudRepository<PersonEntity,Long> {
+    List<PersonEntity> findAllByJobIn(Collection<JobEnum> job);
 }
